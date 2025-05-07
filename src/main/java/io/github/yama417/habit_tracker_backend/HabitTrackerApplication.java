c7046f8d7ca4dac9ce6d5097115482064848d219
YAMA417 <yamanaka.mm417@gmail.com>
2025-05-07 22:38:33 +0900

    add:springbootのひな形作成

---
+++ src/main/java/io/github/yama417/habit_tracker_backend/HabitTrackerApplication.java
@@ -0,0 +1,12 @@
+
+package io.github.yama417.habit_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HabitTrackerApplication.class, args);
+    }
+}
